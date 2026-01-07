70622c8a73b8a3194f2640213f47a0afbcfd94a5
speedcode247 <speedcode247@mail.com>
2026-01-07 09:31:16 +0700

    merge branch

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5nlr9fr12j8"
+    "revision": "0.6g5oq79nfi8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
